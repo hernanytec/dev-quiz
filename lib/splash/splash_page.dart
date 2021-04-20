9147c07c60269c5ca6e358b5cdf74ff058cfbae6
hernany <hernanytec777@gmail.com>
2021-04-19 22:43:20 -0300

    first commit

---
+++ lib/splash/splash_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../core/app_gradients.dart';
+import '../core/app_images.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: AppGradients.linear,
+        ),
+        child: Center(
+          child: Image.asset(AppImages.logo),
+        ),
+      ),
+    );
+  }
+}

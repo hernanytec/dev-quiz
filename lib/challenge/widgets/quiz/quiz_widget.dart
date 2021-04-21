00f2f587c174ccf460f28b4d159b5403b226fa66
hernany <hernanytec777@gmail.com>
2021-04-21 00:32:11 -0300

    adicionando layout da tela de quiz

---
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -0,0 +1,43 @@
+import 'package:DevQuiz/challenge/answer/awnser_widget.dart';
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class QuizWidget extends StatelessWidget {
+  final String title;
+  const QuizWidget({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            title,
+            style: AppTextStyles.heading,
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          AnswerWidgetWidget(
+            title:
+                "Possibilita a criação de aplicativos nativos compilados nativamente.",
+          ),
+          AnswerWidgetWidget(
+            isRight: true,
+            isSelected: true,
+            title:
+                "Possibilita a criação de aplicativos nativos compilados nativamente.",
+          ),
+          AnswerWidgetWidget(
+            title:
+                "Possibilita a criação de aplicativos nativos compilados nativamente.",
+          ),
+          AnswerWidgetWidget(
+            title:
+                "Possibilita a criação de aplicativos nativos compilados nativamente.",
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,6 +1,7 @@
 import 'package:DevQuiz/core/app_colors.dart';
 import 'package:DevQuiz/core/app_images.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/shared/widgets/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
@@ -39,15 +40,7 @@ class QuizCardWidget extends StatelessWidget {
                   style: AppTextStyles.body11,
                 ),
               ),
-              Expanded(
-                flex: 4,
-                child: LinearProgressIndicator(
-                  value: 0.3,
-                  backgroundColor: AppColors.chartSecondary,
-                  valueColor:
-                      AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
-                ),
-              ),
+              Expanded(flex: 4, child: ProgressIndicatorWidget(value: 0.3)),
             ],
           )
         ],

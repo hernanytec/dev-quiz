0b77d495e848883d1e6f7f53e0cb92d6c0abf26f
hernany <hernanytec777@gmail.com>
2021-04-25 16:57:20 -0300

    realizando parse do json e consumindo json local

--- lib/home/home_cotroller.dart
+++ lib/home/home_cotroller.dart
@@ -1,7 +1,5 @@
-import 'package:DevQuiz/core/app_images.dart';
+import 'package:DevQuiz/home/home_repository.dart';
 import 'package:DevQuiz/home/home_state.dart';
-import 'package:DevQuiz/shared/models/answer_model.dart';
-import 'package:DevQuiz/shared/models/question_model.dart';
 import 'package:DevQuiz/shared/models/quiz_model.dart';
 import 'package:DevQuiz/shared/models/user_model.dart';
 import 'package:flutter/foundation.dart';
@@ -14,36 +12,21 @@ class HomeController {
   UserModel? user;
   List<QuizModel>? quizzes;
 
+  final repository = HomeRepository();
+
   void getuser() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 1));
 
-    user = UserModel(
-      name: "Hernany",
-      photoUrl: "https://avatars.githubusercontent.com/u/31345577?v=4",
-    );
+    user = await repository.getUser();
+
     state = HomeState.success;
   }
 
   void getQuizzes() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 1));
 
-    quizzes = [
-      QuizModel(
-        title: "NLW 5 flutter",
-        questions: [
-          QuestionModel(title: "Está curtindo o flutter?", answers: [
-            AnswerModel(title: "Sim! Muito Show!", isRight: true),
-            AnswerModel(title: "Não"),
-            AnswerModel(title: "Mais ou menos"),
-            AnswerModel(title: "Nadinha")
-          ])
-        ],
-        imagem: AppImages.blocks,
-        level: Level.facil,
-      )
-    ];
+    quizzes = await repository.getQuizzes();
+
     state = HomeState.success;
   }
 }

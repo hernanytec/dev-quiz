9097e23548eac0180864dbc07c7eb52558f8c582
hernany <hernanytec777@gmail.com>
2021-04-25 20:43:42 -0300

    adicionando navegacao da splashscreen

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,13 +1,19 @@
+import 'package:dev_quiz/home/home_page.dart';
 import 'package:flutter/material.dart';
 
 import '../core/app_gradients.dart';
 import '../core/app_images.dart';
 
 class SplashPage extends StatelessWidget {
-  const SplashPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 2)).then(
+      (_) => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      ),
+    );
+
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(

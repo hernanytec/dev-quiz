8e76a3829839f97c3bdc139005046167b2e2c624
hernany <hernanytec777@gmail.com>
2021-04-25 23:03:14 -0300

    adicionando animacoes

--- lib/home/widgets/app_bar.dart
+++ lib/home/widgets/app_bar.dart
@@ -51,7 +51,9 @@ class AppBarWidget extends PreferredSize {
                 ),
                 Align(
                   alignment: Alignment(0.0, 1.2),
-                  child: ScoreCardWidget(),
+                  child: ScoreCardWidget(
+                    percent: user.score / 100,
+                  ),
                 )
               ],
             ),
